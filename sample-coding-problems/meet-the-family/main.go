de40291c193643e9f31165e663cf31719d858599
Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>
2021-12-05 13:35:32 +0530

    add some todos for planning
    Signed-off-by: Karuppiah Natarajan <karuppiah7890@users.noreply.github.com>

--- sample-coding-problems/meet-the-family/main.go
+++ sample-coding-problems/meet-the-family/main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello")
+
+	// TODO: Initialize the existing family tree
+
+	// TODO: Take test file path (location) as input from the command line argument
+
+	// TODO: Read the contents of the test file from the test file path
+
+	// TODO: Parse the test file contents to get the various commands and command inputs / arguments
+
+	// TODO: Pass the commands and command inputs to the family tree engine to execute them and get results and/ errors
+
+	// TODO: Get the results and/ errors and print them in proper format
 }
